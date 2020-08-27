b92762c815cebab3f068cc7f2c2c9f79904602bb
abramovvicz <abramovvicz@gmail.com>
2020-08-27 16:44:40 +0200

    commit with spring project

---
+++ src/test/java/com/saidproject/saidproject/SaidprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saidproject.saidproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaidprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
