3bd4c61c17e5a4d2e6f998f07fdca245e68094b9
Bartosz Abramowicz <Bartosz.Abramowicz@harman.com>
2020-10-15 16:13:51 +0200

    add dzejzon

---
+++ src/main/java/com/saidproject/saidproject/utils/GenerateWordFile.java
@@ -0,0 +1,8 @@
+package com.saidproject.saidproject.utils;
+
+import java.io.IOException;
+
+public interface GenerateWordFile {
+
+    void generateWordFile() throws IOException;
+}
---
+++ src/main/java/com/saidproject/saidproject/utils/GenerateWordFileImp.java
@@ -0,0 +1,49 @@
+package com.saidproject.saidproject.utils;
+
+import org.apache.poi.xwpf.usermodel.ParagraphAlignment;
+import org.apache.poi.xwpf.usermodel.UnderlinePatterns;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
+import org.apache.poi.xwpf.usermodel.XWPFRun;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+
+public class GenerateWordFileImp implements GenerateWordFile {
+    public static String output = "rest-with-spring.docx";
+
+    public String convertToString(String fileName){
+        return "";
+    }
+
+    @Override
+    public void generateWordFile() throws IOException {
+        XWPFDocument document = new XWPFDocument();
+        XWPFParagraph title = document.createParagraph();
+        title.setAlignment(ParagraphAlignment.CENTER);
+
+        XWPFRun titleRun = title.createRun();
+        titleRun.setText("Build Your REST API with Spring");
+        titleRun.setColor("009933");
+        titleRun.setBold(true);
+        titleRun.setFontFamily("Courier");
+        titleRun.setFontSize(20);
+
+        XWPFParagraph subTitle = document.createParagraph();
+        subTitle.setAlignment(ParagraphAlignment.CENTER);
+
+        XWPFRun subTitleRun = subTitle.createRun();
+        subTitleRun.setText("from HTTP fundamentals to API Mastery");
+        subTitleRun.setColor("00CC44");
+        subTitleRun.setFontFamily("Courier");
+        subTitleRun.setFontSize(16);
+        subTitleRun.setTextPosition(20);
+        subTitleRun.setUnderline(UnderlinePatterns.DOT_DOT_DASH);
+
+        FileOutputStream out = new FileOutputStream(output);
+        document.write(out);
+        out.close();
+        document.close();
+    }
+}
