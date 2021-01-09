38ade55a10a053af795fd5a6a9335b1494b1aec8
abramovvicz <abramovvicz@gmail.com>
2021-01-09 11:59:27 +0100

    small changes

--- src/test/java/com/saidproject/saidproject/controller/user/LoadContextTest.groovy
+++ src/test/java/com/saidproject/saidproject/controller/user/LoadContextTest.groovy
@@ -1,17 +1,14 @@
 package com.saidproject.saidproject.controller.user;
 
-import com.saidproject.saidproject.controller.user.UserController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import spock.lang.Specification;
-import groovy.lang.*;
-import groovy.util.*;
 
 @SpringBootTest
 class LoadContextTest extends Specification {
 
     @Autowired (required = false)
-    private UserController userController
+    UserController userController
 
     def "when context is loaded then all expected beans are created"() {
         expect: "the UserController is created"
--- src/test/java/com/saidproject/saidproject/controller/user/UserControllerTest.groovy
+++ src/test/java/com/saidproject/saidproject/controller/user/UserControllerTest.groovy
@@ -1,4 +1,36 @@
 package com.saidproject.saidproject.controller.user
 
-class UserControllerTest {
+import com.saidproject.saidproject.service.user.UserService
+import org.mapstruct.Named
+import org.springframework.beans.factory.annotation.Autowired
+import spock.lang.Specification
+
+class UserControllerTest extends Specification {
+
+    @Autowired @Named('userServiceMock')
+    UserService userServiceMock
+
+    void setup() {
+
+    }
+
+    def "FindById"() {
+        when:
+        def result = userServiceMock.testSth()
+
+        then:
+        result == "someUrl/?name=test%"
+    }
+
+    def "FindAll"() {
+    }
+
+    def "Save"() {
+    }
+
+    def "Update"() {
+    }
+
+    def "Delete"() {
+    }
 }
